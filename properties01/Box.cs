879ae01aefd745bc0772cb7af9b0235c1cc19637
Angel Acosta <68080165+MurdockTHS@users.noreply.github.com>
2020-10-10 21:08:13 -0700

    Add project files.

---
+++ properties01/Box.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace properties01
+{
+    class Box
+    {
+        // member variables
+        private int length = 3;
+        private int height;
+        //public int width;
+        private int volume;
+
+        public int Width { get; set; }
+
+        public int Volume
+        {
+            get
+            {
+                return this.length * this.height * this.Width;
+            }
+        }
+
+        /*
+        public int Width
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                this.width = value;
+            }
+        }
+        */
+
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    height = -value;
+                }
+                else
+                {
+                    height = value;
+                }
+            }
+        }
+
+        public Box(int length, int height, int width)
+        {
+            this.length = length;
+            this.height = height;
+            Width = width;
+        }
+
+        public void SetLength(int length)
+        {
+            if(length < 0)
+            {
+                throw new Exception("Length should be higher than 0");
+            }
+            this.length = length;
+        }
+
+        public int GetLength()
+        {
+            return this.length;
+        }
+
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Length is {0} and height is {1} and width is {2} so the volume is {3}", 
+                length, height, Width, volume = length*height*Width);
+        }
+    }
+}
---
+++ properties01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace properties01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box(3, 4, 5);
+            Console.WriteLine("Box s width is " + box.Width);
+            box.Width = 10;
+            Console.WriteLine("Box s width is " + box.Width);
+            Console.WriteLine("Box s volume is " + box.Volume);
+            box.DisplayInfo();
+        }
+    }
+}
